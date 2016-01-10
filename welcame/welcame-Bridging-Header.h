a0c14f9d83c18f2206fbddaceb9a676907e929bf
rawhide.hasegawa <hasegawa@raw-hide.co.jp>
2016-01-10 18:51:46 +0900

    add Bridging-Header

---
+++ welcame/welcame-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  welcame-Bridging-Header.h
+//  welcame
+//
+//  Created by 長谷川　稜 on 2016/01/10.
+//  Copyright © 2016年 ryohasegawa. All rights reserved.
+//
+
+#ifndef welcame_Bridging_Header_h
+#define welcame_Bridging_Header_h
+
+#import <GoogleMaps/GoogleMaps.h>
+#import <SMCalloutView/SMCalloutView.h>
+#import "GADBannerView.h"
+#import "GoogleMapAPIKey.h"
+
+#endif /* welcame_Bridging_Header_h */
