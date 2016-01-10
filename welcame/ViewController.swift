ec769fc4c2902ca88e9fd17b092370f6829f3f21
rawhide.hasegawa <hasegawa@raw-hide.co.jp>
2016-01-10 18:29:00 +0900

    Initial Commit

---
+++ welcame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  welcame
+//
+//  Created by 長谷川　稜 on 2016/01/10.
+//  Copyright © 2016年 ryohasegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
