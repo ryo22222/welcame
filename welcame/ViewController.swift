32e02fe83fde7ead27cef8fa7dff60901ca27bd4
rawhide.hasegawa <hasegawa@raw-hide.co.jp>
2016-01-10 18:54:16 +0900

    import CoreLocation

--- welcame/ViewController.swift
+++ welcame/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate {
+    
+    var calloutview: SMCalloutView?
+    var lm: CLLocationManager!
+    let defaultRadius = 300
+    
+    @IBOutlet var mapview : GMSMapView
+    @IBOutlet var gadbnrview : GADBannerView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapview.myLocationEnabled = true
+        mapview.delegate = self
+        
+        lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.requestAlwaysAuthorization()
+        lm.distanceFilter = 300
+        lm.startUpdatingLocation()
+        //startLocation()
     }
-
+    
+    func startLocation(){
+        //NSLog("aaaaaa")
+        lm.startUpdatingLocation()
+    }
+    
+    func stopLocation(){
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!){
+        //NSLog("bbb")
+        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:newLocation.coordinate.latitude,longitude:newLocation.coordinate.longitude)
+        var now :GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(coordinate.latitude,longitude:coordinate.longitude,zoom:17)
+        mapview.camera = now
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
+        //NSLog("ccc")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
