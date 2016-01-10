d7b448c7f03590f73a6a21accd7dfaf0217e3148
rawhide.hasegawa <hasegawa@raw-hide.co.jp>
2016-01-10 20:06:33 +0900

    fix

---
+++ welcame/welcame-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  welcame-Bridging-Header.h
+//  welcame
+//
+//  Created by 長谷川　稜 on 2016/01/10.
+//  Copyright © 2016年 ryohasegawa. All rights reserved.
+//
+
+#import <GoogleMaps/GoogleMaps.h>
