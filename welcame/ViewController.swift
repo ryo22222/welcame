b9d372e03e0d0e96d6e3f5972c1772cc5ca8dcf2
rawhide.hasegawa <hasegawa@raw-hide.co.jp>
2016-01-10 21:37:43 +0900

    ページ遷移実装

--- welcame/ViewController.swift
+++ welcame/ViewController.swift
@@ -11,37 +11,25 @@ import GoogleMaps
 
 class ViewController: UIViewController, GMSMapViewDelegate {
     
-    var gmaps : GMSMapView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // MapViewを生成する.
-        let camera = GMSCameraPosition.cameraWithLatitude(35.658581,longitude: 139.745433, zoom: 10)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
-        
-        // 東京タワー
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(35.658581, 139.745433)
-        marker.title = "ピンに出るタイトル"
-        marker.snippet = "ピンに出る詳細"
-        
-        // 六本木ヒルズ
-        let marker2 = GMSMarker()
-        marker2.position = CLLocationCoordinate2DMake(35.660464, 139.729249)
-        marker2.title = "ピンに出るタイトル"
-        marker2.snippet = "ピンに出る詳細"
-        
-        // MapViewをviewに追加する.
-        marker.map = mapView
-        marker2.map = mapView
-        
+        // buttonを設置
+        let btn = UIButton(frame: CGRectMake(0, 0, 100, 30))
+        btn.setTitle("Push", forState: UIControlState.Normal)
+        btn.backgroundColor = UIColor.redColor()
+        btn.layer.position = CGPoint(x: 100, y: 100)
+        btn.addTarget(self, action: "onClick", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(btn)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // buttonをタップしたときのアクション
+    func onClick() {
+        let second = gmapViewController()
+        self.presentViewController(second, animated: true, completion: nil)
     }
 }
